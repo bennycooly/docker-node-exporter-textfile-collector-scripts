4dc1239e76c33a144d04aa325e1598e661a0e09f
Ben Fu <ben@benfu.me>
2021-01-14 20:50:06 -0600

    Fix syntax for interval

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,7 +2,7 @@
 
 COMMAND=${COMMAND:-'echo "Hello world"'}
 
-INTERVAL=${INTERVAL:'*/5 * * * *'}
+INTERVAL=${INTERVAL:-'*/5 * * * *'}
 
 echo "$INTERVAL root $COMMAND" > /proc/1/fd/1 2>/proc/1/fd/2
 
